19673cc431ce501c06151e443e1d7b9ed715ca92
NikSkopin <skopin.n@gmail.com>
2019-06-29 19:51:18 +0200

    11 video player finished

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,46 @@
+const myVideo = document.querySelector(".player__video");
+const playBtn = document.querySelector(".playbutton");
+const videoVolume = document.querySelector('input[name="volume"]');
+const videoSpeed = document.querySelector('input[name="playbackRate"]');
+const currentTime = document.querySelector(".progress__filled");
+const rewind = document.querySelectorAll("button[data-skip]");
+const progressBar = document.querySelector(".progress");
+
+function skipTime() {
+  if (myVideo.currentTime != myVideo.duration) {
+    myVideo.currentTime += +this.dataset.skip;
+  }
+}
+
+myVideo.addEventListener("timeupdate", () => {
+  currentTime.style.flexBasis = `${(myVideo.currentTime / myVideo.duration) *
+    100}%`;
+});
+
+rewind.forEach(button => {
+  button.addEventListener("click", skipTime);
+});
+
+playBtn.addEventListener("click", () => {
+  if (myVideo.paused) {
+    myVideo.play();
+  } else {
+    myVideo.pause();
+  }
+});
+
+videoVolume.addEventListener("input", () => {
+  myVideo.volume = videoVolume.value;
+});
+videoSpeed.addEventListener("input", () => {
+  myVideo.playbackRate = videoSpeed.value;
+});
+
+progressBar.addEventListener("click", e => {
+  let xPosition = e.clientX;
+  let barWidth = progressBar.clientWidth;
+
+  let clickPosition = xPosition / barWidth;
+
+  myVideo.currentTime = myVideo.duration * clickPosition;
+});
